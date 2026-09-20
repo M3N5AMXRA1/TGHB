6ceed4d6c073b9404e497bdf423b75a99622b7d1
M3N5AMXRA1 <96667933+M3N5AMXRA1@users.noreply.github.com>
2026-09-20 21:15:11 +0500

    bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "stalking",     // this is what people actually see
+        state: "Наелся грибов и слушаю CUPSIZE",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
