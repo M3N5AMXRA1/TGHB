e933d2ebb18c0e17f19a99d2635e4735156b122a
M3N5AMXRA1 <96667933+M3N5AMXRA1@users.noreply.github.com>
2026-09-21 02:41:52 +0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -93,7 +93,7 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
+      primary: "#ff7e21",
       secondary: "#2F3136",
 
       // Standard status colors for success/error/warning/info messages.
